183c22cf94961a63931501ba50573d5f8a898c0c
Ivan Ajenjo Vicente <ivan.ajenjo.vicente@gmail.com>
2019-06-12 18:31:53 +0200

    Update instantGaming.py
    commit3

--- instantGaming.py
+++ instantGaming.py
@@ -1,13 +1,19 @@
 # Import libraries
 import requests
-import urllib.request
+from urllib.request import urlopen, Request
 import time
 from bs4 import BeautifulSoup
 
 print("Hola")
 
 # Set the URL you want to webscrape from
-url = 'https://www.instant-gaming.com/es/'
+url = "https://www.instant-gaming.com/es/"
+req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
 
+juego = input("Que juego quiere buscar?")
+print("Quiere buscar " + juego)
 
+html = urlopen(req)
+res = BeautifulSoup(html.read(),"html.parser")
 
+print(res.title)
