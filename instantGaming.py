78247c1bd6a053626de133a69933122c0a600fab
Ivan Ajenjo Vicente <ivan.ajenjo.vicente@gmail.com>
2019-06-12 18:19:10 +0200

    2nd commit
    Testing 2nd commit

--- instantGaming.py
+++ instantGaming.py
@@ -1,1 +1,13 @@
+# Import libraries
+import requests
+import urllib.request
+import time
+from bs4 import BeautifulSoup
+
 print("Hola")
+
+# Set the URL you want to webscrape from
+url = 'https://www.instant-gaming.com/es/'
+
+
+
