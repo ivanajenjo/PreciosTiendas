0fcf877e5795c26332d1d39505eb7b7b6c62f9b9
Ivan Ajenjo Vicente <ivan.ajenjo.vicente@gmail.com>
2019-06-12 18:52:02 +0200

    Update instantGaming.py

--- instantGaming.py
+++ instantGaming.py
@@ -1,6 +1,8 @@
 # Import libraries
-import requests
+from requests import get
 from urllib.request import urlopen, Request
+from requests.exceptions import RequestException
+from contextlib import closing
 import time
 from bs4 import BeautifulSoup
 
@@ -17,3 +19,38 @@ html = urlopen(req)
 res = BeautifulSoup(html.read(),"html.parser")
 
 print(res.title)
+
+urlbusqueda = "https://www.instant-gaming.com/es/busquedas/?query=" + juego
+req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+
+html = urlopen(req)
+res = BeautifulSoup(html.read(),"html.parser")
+print(res.find_all('#ig-panel-center > div.search-wrapper > div.search > div:nth-child(1)'))
+
+print(res.title)
+
+
+
+
+def simple_get(url):
+    try:
+        with closing(get(url, stream=True)) as resp:
+            if is_good_response(resp):
+                return resp.content
+            else:
+                return None
+
+    except RequestException as e:
+        log_error('Error during requests to {0} : {1}'.format(url, str(e)))
+        return None
+
+
+def is_good_response(resp):
+    content_type = resp.headers['Content-Type'].lower()
+    return (resp.status_code == 200 
+            and content_type is not None 
+            and content_type.find('html') > -1)
+
+
+def log_error(e):
+    print(e)
