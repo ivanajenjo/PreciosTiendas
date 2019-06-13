3d8782ca8b6a1279cf9ea17f6c747439a269e4f2
Ivan Ajenjo Vicente <37843278+ivanajenjo@users.noreply.github.com>
2019-06-13 19:07:41 +0200

    g2a integration test

---
+++ g2a.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+
+def buscarG2A(juego):
+    navegador = webdriver.Chrome("driver/chromedriver.exe")
+    ##navegador.minimize_window()
+    navegador.get("https://www.g2a.com/")
+    elem = navegador.find_element_by_class_name("btn btn-primary")
+    elem.click()
+    elem = navegador.find_element_by_name("query")
+    elem.clear()
+    elem.send_keys(juego)
+    elem.send_keys(Keys.RETURN)
+    elem = navegador.find_element_by_class_name("Card__price-cost price")
+    print("Precio en G2A de ", juego, " es ", elem.text)
+    navegador.close()
+
+buscarG2A("GTAV")
